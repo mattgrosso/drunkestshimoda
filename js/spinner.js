7d5fd0e0a0d64b62adc393632cc11a24692c17a1
mattgrosso <mattgrosso@gmail.com>
2018-01-30 10:43:01 -0500

    Style button

--- js/spinner.js
+++ js/spinner.js
@@ -4,6 +4,11 @@
   var randomDegrees = Math.floor(Math.random() * 1500) + 1000;
 
   $('.spin-button').click(function (event) {
+    $(this).addClass('clicking');
+    setTimeout(function() {
+      $('.spin-button').removeClass('clicking');
+    }, 100);
+    $(this).addClass('clicking');
     randomDegrees = randomDegrees + Math.floor(Math.random() * 1500) + 1000;
     $('.spinner').css('transform', `rotate(${randomDegrees}deg)`)
   })
